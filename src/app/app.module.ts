22726829fed5436873ebfd3af3af1483e691f35e
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-09-29 23:04:01 +0800

    Header done and its responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule
